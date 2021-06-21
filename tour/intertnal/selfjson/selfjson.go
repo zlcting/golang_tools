c1f5ad682fdd7cf75f586400a110fad871370eff
longchuan <93-longchuan@users.noreply.gitlab.leju.com>
2021-06-21 20:23:24 +0800

    json

--- tour/intertnal/selfjson/selfjson.go
+++ tour/intertnal/selfjson/selfjson.go
@@ -2,7 +2,25 @@ package selfjson
 
 import "encoding/json"
 
+// Gaudedistrict https://mholt.github.io/json-to-go/ 自动生成json对应的struct
 type Gaudedistrict struct {
+	Status     string `json:"status"`
+	Info       string `json:"info"`
+	Infocode   string `json:"infocode"`
+	Count      string `json:"count"`
+	Suggestion struct {
+		Keywords []interface{} `json:"keywords"`
+		Cities   []interface{} `json:"cities"`
+	} `json:"suggestion"`
+	Districts []struct {
+		Citycode  string        `json:"citycode"`
+		Adcode    string        `json:"adcode"`
+		Name      string        `json:"name"`
+		Polyline  string        `json:"polyline"`
+		Center    string        `json:"center"`
+		Level     string        `json:"level"`
+		Districts []interface{} `json:"districts"`
+	} `json:"districts"`
 }
 
 func Jsonmap(str string) map[string]interface{} {
